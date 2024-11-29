c166091acd1d7e88f149e9fa3364988b02b38cb3
In Tae Chung <intae29@gmail.com>
2024-11-28 20:20:14 -0500

    added key attribute to pagination buttons

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -1,62 +1,62 @@
-import useSWR from 'swr';
-import { useState, useEffect } from 'react';
-import { Card, Col, Container, Pagination, Row } from 'react-bootstrap';
-import { useRouter } from 'next/router';
-import ArtworkCard from '@/components/ArtworkCard';
-import Error from 'next/error';
-import validObjectIDList from '@/public/data/validObjectIDList.json'
+import useSWR from "swr";
+import { useState, useEffect } from "react";
+import { Card, Col, Pagination, Row } from "react-bootstrap";
+import { useRouter } from "next/router";
+import ArtworkCard from "@/components/ArtworkCard";
+import Error from "next/error";
+import validObjectIDList from "@/public/data/validObjectIDList.json";
 
-const PER_PAGE = 12;
+const PER_PAGE = 8;
 
 export default function Artwork() {
-
   const [artworkList, setArtworkList] = useState();
   const router = useRouter();
   const [page, setPage] = useState(1);
 
-  let finalQuery = router.asPath.split('?')[1];
+  let finalQuery = router.asPath.split("?")[1];
 
-  const { data, error } = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/search?${finalQuery}`);
+  const { data, error } = useSWR(
+    `https://collectionapi.metmuseum.org/public/collection/v1/search?${finalQuery}`
+  );
 
   function handlePage(page) {
-    setPage(p => page);
+    setPage((p) => page);
   }
 
   useEffect(() => {
-
     if (data) {
-      let filteredResults = validObjectIDList.objectIDs.filter(x => data.objectIDs?.includes(x));
+      let filteredResults = validObjectIDList.objectIDs.filter((x) =>
+        data.objectIDs?.includes(x)
+      );
       const results = [];
 
       for (let i = 0; i < filteredResults.length; i += PER_PAGE) {
         const chunk = filteredResults.slice(i, i + PER_PAGE);
         results.push(chunk);
-        }
+      }
 
       setArtworkList(results);
       setPage(1);
-
     }
-
-  }, [data])
+  }, [data]);
 
   if (error) {
-    return <Error statusCode={404} />
+    return <Error statusCode={404} />;
   }
 
   if (artworkList) {
-    let lastPage = artworkList.length
+    let lastPage = artworkList.length;
     return (
       <>
-
-        {lastPage > 0 ?
-
-          <Row className="gy-4">{artworkList[page - 1]?.map(objID => (
-            <Col lg={3} sm={6} key={objID}><ArtworkCard objectID={objID} /></Col>
-          ))}</Row>
-
-          :
-
+        {lastPage > 0 ? (
+          <Row className="gy-4">
+            {artworkList[page - 1]?.map((objID) => (
+              <Col lg={3} sm={6} key={objID}>
+                <ArtworkCard objectID={objID} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
           <Card>
             <Card.Body>
               <Card.Text>
@@ -64,32 +64,62 @@ export default function Artwork() {
               </Card.Text>
             </Card.Body>
           </Card>
+        )}
 
-        }
+        {lastPage > 0 && (
+          <Row>
+            <Col className="d-flex justify-content-center p-4">
+              <br />
+              <Pagination>
+                {page > 2 && <Pagination.First onClick={() => handlePage(1)} key="first" />}
+                {page > 1 && (
+                  <Pagination.Prev onClick={() => handlePage(page - 1)} key="prev" />
+                )}
 
-        {lastPage > 0 && <Row>
+                {page > 2 && (
+                  <Pagination.Item onClick={() => handlePage(page - 2)} key={page - 2} >
+                    {page - 2}
+                  </Pagination.Item>
+                )}
+                {page > 1 && (
+                  <Pagination.Item onClick={() => handlePage(page - 1)} key={page - 1} >
+                    {page - 1}
+                  </Pagination.Item>
+                )}
 
-          <Col className="d-flex justify-content-center p-4">
-            <br />
-            <Pagination>
-              {page > 2 && <Pagination.First onClick={() => handlePage(1)} />}
-              {page > 1 && <Pagination.Prev onClick={() => handlePage(page - 1)} />}
+                {/* Current Page */}
+                <Pagination.Item
+                  active
+                  tabIndex={0}
+                  aria-current="page"
+                  key={page}
+                >
+                  {page}
+                </Pagination.Item>
 
-              {page > 2 && <Pagination.Item onClick={() => handlePage(page - 2)}>{page - 2}</Pagination.Item>}
-              {page > 1 && <Pagination.Item onClick={() => handlePage(page - 1)}>{page - 1}</Pagination.Item>}
-              <Pagination.Item className="active">{page}</Pagination.Item>
-              {page < lastPage && <Pagination.Item onClick={() => handlePage(page + 1)}>{page + 1}</Pagination.Item>}
-              {page < lastPage-1 && <Pagination.Item onClick={() => handlePage(page + 2)}>{page + 2}</Pagination.Item>}
+                {page < lastPage && (
+                  <Pagination.Item onClick={() => handlePage(page + 1)} key={page + 1} >
+                    {page + 1}
+                  </Pagination.Item>
+                )}
+                {page < lastPage - 1 && (
+                  <Pagination.Item onClick={() => handlePage(page + 2)} key={page + 2} >
+                    {page + 2}
+                  </Pagination.Item>
+                )}
 
-              {page < lastPage && <Pagination.Next onClick={() => handlePage(page + 1)} />}
-              {page < lastPage-1 && <Pagination.Last onClick={() => handlePage(lastPage)} />}
-            </Pagination>
-          </Col>
-        </Row>
-
-        }
+                {page < lastPage && (
+                  <Pagination.Next onClick={() => handlePage(page + 1)} key="next" />
+                )}
+                {page < lastPage - 1 && (
+                  <Pagination.Last onClick={() => handlePage(lastPage)} key="last" />
+                )}
+              </Pagination>
+            </Col>
+          </Row>
+        )}
       </>
-    )
+    );
   } else {
     return null;
   }
